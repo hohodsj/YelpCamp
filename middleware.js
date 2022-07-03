97d2436aeb97f8bfb2993abd39df079112d2a6f7
hohodsj <hohodsj@gmail.com>
2022-07-03 09:10:15 -0400

    authorization

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const Campground = require('./models/campground');
+const Review = require('./models/review');
+const {campgroundSchema, reviewSchema} = require('./schemas');
+
+// Check if user is login
 module.exports.isLoggedIn = (req, res, next) => {
     console.log(`REQ.USER:${req.user}`);
     if(!req.isAuthenticated()) {
@@ -9,3 +14,46 @@ module.exports.isLoggedIn = (req, res, next) => {
     next();
 }
 
+// Check if camground passed in is valid ex. if required fields are being passed in
+module.exports.validateCampground = (req, res, next) => {
+    const {error} = campgroundSchema.validate(req.body)
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+};
+
+// Check if campground created by the login user
+module.exports.isAuthor = async(req, res, next) => {
+    const {id} = req.params; // fetch campground id
+    const campground = await Campground.findById(id); //fetch campground object
+    if(!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+// if review passed in is valid
+module.exports.validateReview = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+// Check if review created by the login user
+module.exports.isReviewAuthor = async(req, res, next) => {
+    const {id, reviewId} = req.params; // fetch review id from reviews.js (route)
+    const review = await Review.findById(reviewId); //fetch review object
+    if(!review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
